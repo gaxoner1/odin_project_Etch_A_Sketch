256e7a60536640afda83ca30e01737671d34494c
Geoffrey Andrade <geoffandrade@hotmail.com>
2020-05-20 12:41:13 -0700

    Improved button functionality to reset grid

---
+++ README.md
@@ -0,0 +1,4 @@
+# Etch a Sketch Project
+
+Etch a Sketch using HTMl, CSS & Vanilla JS to dynamically create a grid and trigger pixelated drawing based on mouse movement.
+
--- script.js
+++ script.js
@@ -1,29 +1,46 @@
-let gridSize = 16;
+let gridSize = 16
 let gridTotal = gridSize ** 2;
-
+const container = document.querySelector(".grid-container");
 
 //create grid w/ divs
 function createDivs() {
-  for (i = 0; i < gridTotal; i++){
+  let gridTotal = gridSize ** 2;
+  for (let i = 0; i < gridTotal; i++){
     let newDiv = document.createElement("div");
-    const container = document.getElementsByClassName("grid-container");
-    newDiv.className = "grid-item";
-    container[0].appendChild(newDiv);
+    newDiv.classList.add("grid-item");
+    container.appendChild(newDiv);
+    container.style.setProperty('grid-template-columns', 'repeat(' + gridSize + ', 1fr)');
   }
 }
 createDivs();
 
-
-//draw: add event listener: change BG:
+// //draw: add event listener: change BG:
 let userInteract = document.querySelector("#gridBody");
 userInteract.addEventListener("mouseover", draw);
-
+//
 function draw(e) {
-//e stores the element that eventListener triggers (ie.div.grid-item is target)
-//so we can check target is not parent element we assigned (ie.div #gridBody)
-  if (e.target != e.currentTarget) {
-    // console.log(e.currentTarget) <--checks for reference
-    // console.log(e.target); <-- checks for reference
-    e.target.style.background = 'blue'
+ /* e stores the element that eventListener triggers (ie.div.grid-item is target)
+ so we can check target is not parent element we assigned (ie.div #gridBody) */
+   if (e.target != e.currentTarget) {
+     e.target.style.background = '#9932CC'
+   }
+ }
+
+let button = document.querySelector(".button");
+button.addEventListener("click", resetGrid);
+
+function resetGrid() {
+  let userChoice = prompt("Enter grid size:");
+  let newSize = parseInt(userChoice);
+  console.log(newSize)
+  if (newSize != null) {
+    let gridAll = document.querySelectorAll("div.grid-item");
+    console.log(gridAll)
+    gridAll.forEach(function (cell) {
+        cell.remove();
+    });
+    gridSize = newSize
+    gridTotal = gridSize ** 2;
+    createDivs()
   }
 }
